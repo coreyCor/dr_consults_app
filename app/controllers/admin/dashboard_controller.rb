876cec6dd967424ef4325c862320216172d1c636
ccutlip <ccutlip@datakill.local>
2026-07-26 21:04:14 -0700

    Sidebar scroll bootsrap update - admin dash removal of a's -

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -8,7 +8,6 @@ class Admin::DashboardController < ApplicationController
 
     today = Time.current.beginning_of_day..Time.current.end_of_day
     @total_today   = Consult.where(created_at: today).count
-    @answered      = Consult.answered.count
     @fbx_neos_today = Consult.where(consult_type: "fbx_neo", created_at: today).count
   end
 end
